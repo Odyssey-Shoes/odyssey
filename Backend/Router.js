5a83d4f97b3270aedf11192fb789aa88db0e464d
sachinNishalka <nishalkasachin@gmail.com>
2023-09-18 21:58:32 +0530

    backend create data

--- Backend/Router.js
+++ Backend/Router.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const allShoes = require("./controllers/allshoes");
-const sandles = require("./controllers/sandles");
+const addshoe = require("./controllers/addshoe");
 const path = require("path");
 
 filePath = path.join(__dirname, "shoedata.json");
-
-router.route("/").get(allShoes);
-router.route("/sandles/").get(sandles);
+router.route("/api/v1/addshoe/").post(addshoe);
+router.route("/api/v1/allshoes").get(allShoes);
 
 module.exports = router;
