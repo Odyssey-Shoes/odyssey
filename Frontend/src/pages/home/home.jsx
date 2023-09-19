538bd54b72a1fa583cb2427bad8c03c7711feaa5
Geeth <induwarakwg3573@gmail.com>
2023-09-19 23:43:20 +0530

    Updated

--- Frontend/src/pages/home/home.jsx
+++ Frontend/src/pages/home/home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function home() {
   return (
     <div>
-      <br /><br /><br /><br /><br /><br />
+      <br /><br /><br /><br /><br /><br /><br />
       <h1>Hello from home</h1>
     </div>
   
