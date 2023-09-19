49eb00cfcc13a12d0a8b330040c9cc88f4d8dab6
Damitha <hellodamitha@gmail.com>
2023-09-19 17:13:13 +0530

    navbar Updated

--- Frontend/src/components/navbar/navbar.jsx
+++ Frontend/src/components/navbar/navbar.jsx
@@ -4,23 +4,80 @@ import headerLogo from "/assets/headerLogo.svg"; //absolute path for the same im
 
 const Navbar = () => {
   return (
-    <header className="absolute">
-      <div className="flex items-center justify-between py-4">
-        <img
-          className="pl-8 "
-          src={headerLogo}
-          style={{ height: "38px" }}
-          alt=""
-        />
+    <header>
+      <nav class="bg-white  fixed w-full z-20 top-10 left-0 border-b border-gray-200 ">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="#" class="flex items-center">
+            <img src={headerLogo} class="h-8 mr-3" alt="Logo"></img>
+          </a>
 
-        <ul className="flex items-center justify-between flex-1 gap-16 text-xl font-bold pl-80 nav-links max-lg:hidden">
-          <li className="nav-link">Home</li>
-          <li className="nav-link">Men</li>
-          <li className="nav-link">Women</li>
-          <li className="nav-link">Kids</li>
-          <li className="nav-link">Sports</li>
-        </ul>
-      </div>
+          <div class="flex md:order-2">
+            <button
+              type="button"
+              class="text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 "
+            >
+              Login
+            </button>
+          </div>
+
+          <div
+            class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            id="navbar-sticky"
+          >
+            <ul class="flex text-lg flex-col p-4 md:p-0 mt-4 font-semibold border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 "
+                  aria-current="page"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 "
+                >
+                  Men
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 "
+                >
+                  Women
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 "
+                >
+                  Kids
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 "
+                >
+                  Sports
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 "
+                >
+                  Sales
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </header>
   );
 };
--- Frontend/src/components/topNavbar/topNavbar.jsx
+++ Frontend/src/components/topNavbar/topNavbar.jsx
@@ -1,6 +1,6 @@
 function TopNavbar() {
   return (
-    <div className="bg-[#023047] p-2">
+    <div className="p-2 bg-primary">
       <h1 className="text-center text-white">
         Get 50% Off For Selected Items | Shop Now
       </h1>
