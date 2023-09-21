9eb95b7fbba07c5fe7fe132924ade8018c3a0f57
Damitha <hellodamitha@gmail.com>
2023-09-21 21:08:56 +0530

    import file path edited

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 //import { useState } from "react";
 import TopNavbar from "./components/topNavbar/topNavbar";
-import Navbar from "./components/navbar/NavBar";
+import Navbar from "./components/navbar/navbar";
 
 function App() {
   //const [count, setCount] = useState(0);
