88e7c0c019475a63f86db5ac4a469f12d3c9a1f4
Geeth <induwarakwg3573@gmail.com>
2023-09-22 21:38:44 +0530

    registration form completed

--- Frontend/src/pages/login/login.jsx
+++ Frontend/src/pages/login/login.jsx
@@ -7,17 +7,13 @@ const Login = () => {
     <div>
       <section class="bg-gray-50 pt-10">
         <div class="flex flex-col items-center justify-center px-6 mx-auto md:h-screen lg:py-0">
-          <Link
-            to="/"
-            class="flex items-center mb-6 text-2xl font-semibold text-gray-900 "
-          >
+          <div class="flex items-center mb-6 text-2xl font-semibold text-gray-900">
             <img class="h-12 mr-2" src={headerLogo} alt="logo" />
-          </Link>
-
+          </div>
           <div class="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 ">
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
-                Sign in to your account
+                Log in to your account
               </h1>
               <form class="space-y-4 md:space-y-6" action="#">
                 <div>
@@ -32,7 +28,7 @@ const Login = () => {
                     name="email"
                     id="email"
                     class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                    placeholder="name@company.com"
+                    placeholder="name@gmail.com"
                     required=""
                   />
                 </div>
@@ -84,12 +80,11 @@ const Login = () => {
                 </button>
                 <p class="text-sm font-light text-gray-500 ">
                   Don’t have an account yet?{" "}
-                  <a
-                    href="#"
+                  <Link to="/Signup"
                     class="font-medium text-primary-600 hover:underline"
                   >
                     Sign up
-                  </a>
+                  </Link>
                 </p>
               </form>
             </div>
