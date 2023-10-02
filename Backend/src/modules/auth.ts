c0f83d0af656fe32a723f76ef0b9bbc090fb2636
sachinNishalka <nishalkasachin@gmail.com>
2023-10-02 09:33:42 +0530

    added the data

--- Backend/src/modules/auth.ts
+++ Backend/src/modules/auth.ts
@@ -2,4 +2,10 @@ import jwt from "jsonwebtoken";
 
 // this function will creates a jwt token for user object when it passes to the function
 
-export const createJWT = (user) => {};
+export const createJWT = (user) => {
+  const token = jwt.sign(
+    { id: user.id, username: user.username },
+    process.env.JWT_SECRET
+  );
+  return token;
+};
