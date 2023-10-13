663d5d2e701015ab793efbb4dc0286799bf2edaa
Damitha <hellodamitha@gmail.com>
2023-10-13 15:19:17 +0530

    spelling mistake

--- Frontend/src/pages/signup/signup.jsx
+++ Frontend/src/pages/signup/signup.jsx
@@ -13,7 +13,7 @@ const Signup = () => {
           <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 ">
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
-                Create and account
+                Create an account
               </h1>
               <form class="space-y-4 md:space-y-6" action="#">
                 <div>
